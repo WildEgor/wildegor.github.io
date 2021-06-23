648f86053b9e119832697364f2c9cf7ccfd74b17
Egor Kartashov <68367096+WildEgor@users.noreply.github.com>
2021-06-23 12:22:07 +0600

    delete

--- CheatSheets/basicAlgorithms/TS/src/components/Playground/index.tsx
+++ CheatSheets/basicAlgorithms/TS/src/components/Playground/index.tsx
@@ -1,16 +1,4 @@
 import React from "react";
-import { LinkedList } from "../../utils/LinkedList";
-import { BitMath } from "../../utils/Bits";
-
-const list = new LinkedList();
-list.add(5);
-list.add(3);
-list.getIndexOf(5);
-list.print();
-
-console.log(BitMath.getBit(5, 2));
-console.log(BitMath.setBit(4, 0));
-console.log(BitMath.updateBit(5, 0, 0));
 
 const Playground = () => {
   return <div></div>;
