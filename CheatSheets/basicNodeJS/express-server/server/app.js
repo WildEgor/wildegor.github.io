df3725e879ab8d37af22d9fab434b0445e24f969
Egor Kartashov <68367096+WildEgor@users.noreply.github.com>
2021-07-19 18:20:39 +0600

    up

--- CheatSheets/basicNodeJS/express-server/server/app.js
+++ CheatSheets/basicNodeJS/express-server/server/app.js
@@ -1,15 +1,48 @@
 const express = require('express')
 const path = require('path')
+const { products } = require('./data')
+
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().getFullYear();
+    console.log(method, url, time);
+    next()
+}
+
 const app = express()
 
 app.use(express.static(path.resolve(__dirname, './public')))
+app.use('/api', logger);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './index.html'))
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map(product => { 
+        return {
+            id: product.id,
+            order: product.order,
+            price: product.price, 
+            name: products.name, 
+            // desc: product.desc 
+        }
+    })
+
+    res.json(newProducts);
 })
 
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>404</h1>')
+app.get('/api/products/:id', (req, res) => {
+    console.log(req.params);
+    const singleProd = products.find(product => product.id === Number(req.params.id))
+
+    res.json(singleProd);
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params)
+    res.send('Review');
+})
+
+app.get('/home', (req, res) => {
+    res.send('Home page middleware')
 })
 
 app.listen(5000, () => {
---
+++ CheatSheets/basicNodeJS/express-server/server/data.js
@@ -0,0 +1,27 @@
+const products = [
+    {
+        id: 0,
+        order: 1,
+        price: 10,
+        name: 'product 1',
+        desc: 'product 1 desc'
+    },
+    {
+        id: 1,
+        order: 3,
+        price: 30,
+        name: 'product 2',
+        desc: 'product 2 desc'
+    },
+    {
+        id: 2,
+        order: 2,
+        price: 20,
+        name: 'product 3',
+        desc: 'product 3 desc'
+    }
+];
+
+module.exports = {
+    products
+}
